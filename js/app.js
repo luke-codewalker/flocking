ca5174d12f357e9a6a116f0f1e9702589550bcf9
Luke Codewalker <arathorn089@arcor.de>
2018-06-24 23:21:46 +0200

    Made site mobile-friendly
    Made the site mobile friendly by ading a media query that changes the layout of the menu to reveal more of the canvas.

--- js/app.js
+++ js/app.js
@@ -1,38 +1,40 @@
-const canvas = select('#canvas');
-const ctx = canvas.getContext('2d');
+const canvas = select("#canvas");
+const ctx = canvas.getContext("2d");
 
-const separationInput = select('#separation');
-const cohesionInput = select('#cohesion');
-const alignmentInput = select('#alignment');
+const separationInput = select("#separation");
+const cohesionInput = select("#cohesion");
+const alignmentInput = select("#alignment");
 
-const frameRateDisplay = select('.frameRate');
-const vehicleDisplay = select('.vehicles');
+const frameRateDisplay = select(".frameRate");
+const vehicleDisplay = select(".vehicles");
 
-const clearBtn = select('#clear');
+const clearBtn = select("#clear");
 
 const vehicles = [];
 
-const colorSelect = select('#colors');
+const colorSelect = select("#colors");
 for (const colorTheme in colors) {
   if (colors.hasOwnProperty(colorTheme)) {
     const theme = colors[colorTheme];
-    const option = document.createElement('option');
-    option.setAttribute('value', colorTheme);
-    if(colorTheme == 'default') {
-      option.setAttribute('selected', '');
+    const option = document.createElement("option");
+    option.setAttribute("value", colorTheme);
+    if (colorTheme == "default") {
+      option.setAttribute("selected", "");
     }
-    option.innerText = `${colorTheme[0].toUpperCase()}${colorTheme.substr(1)}`;
+    option.textContent = `${colorTheme[0].toUpperCase()}${colorTheme.substr(
+      1
+    )}`;
     colorSelect.appendChild(option);
   }
 }
 
 let currentScheme;
-if(!colors[currentScheme]) currentScheme = 'default';
+if (!colors[currentScheme]) currentScheme = "default";
 
-colorSelect.addEventListener('change', (e) => {
+colorSelect.addEventListener("change", e => {
   currentScheme = e.target.value;
-  if(!colors[currentScheme]) currentScheme = 'default';
-})
+  if (!colors[currentScheme]) currentScheme = "default";
+});
 
 let clearedRecently = false;
 let clearTime;
@@ -43,7 +45,7 @@ const maxFrameRate = 40;
 let lastTime = Date.now();
 
 function draw() {
-  if(clearedRecently && Date.now() - clearTime < 800) {
+  if (clearedRecently && Date.now() - clearTime < 800) {
     ctx.fillStyle = colors[currentScheme].clear;
   } else {
     ctx.fillStyle = colors[currentScheme].bg;
@@ -52,12 +54,12 @@ function draw() {
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 
   for (const vehicle of vehicles) {
-    if(vehicle.position.x > canvas.width + vehicle.length) {
-      vehicle.position.x = - vehicle.length;
+    if (vehicle.position.x > canvas.width + vehicle.length) {
+      vehicle.position.x = -vehicle.length;
     } else if (vehicle.position.x < 0 - vehicle.length) {
       vehicle.position.x = canvas.width + vehicle.length;
     } else if (vehicle.position.y > canvas.height + vehicle.length) {
-      vehicle.position.y = - vehicle.length;
+      vehicle.position.y = -vehicle.length;
     } else if (vehicle.position.y < 0 - vehicle.length) {
       vehicle.position.y = canvas.height + vehicle.length;
     }
@@ -69,9 +71,9 @@ function draw() {
     vehicle.display(colors[currentScheme].stroke, colors[currentScheme].fill);
   }
 
-  frameRateDisplay.innerText = frameRate;
+  frameRateDisplay.textContent = ` ${frameRate}`;
 
-  if(Date.now() - lastTime < 1000) {
+  if (Date.now() - lastTime < 1000) {
     frameCount++;
   } else {
     frameRate = frameCount;
@@ -79,47 +81,45 @@ function draw() {
     lastTime = Date.now();
   }
 
-  vehicleDisplay.innerText = vehicles.length;
+  vehicleDisplay.textContent = ` ${vehicles.length}`;
 
   window.requestAnimationFrame(draw);
 }
 
 draw();
 
-canvas.addEventListener('click', (e) => {
-  if(frameRate > maxFrameRate) {
+canvas.addEventListener("click", e => {
+  if (frameRate > maxFrameRate) {
     vehicles.push(new Vehicle(e.offsetX, e.offsetY));
   }
 });
 
-canvas.addEventListener('dblclick', (e) => {
-  if(frameRate > maxFrameRate) {
-    for(let i = 0; i < 10; i++) {
+canvas.addEventListener("dblclick", e => {
+  if (frameRate > maxFrameRate) {
+    for (let i = 0; i < 10; i++) {
       vehicles.push(new Vehicle(e.offsetX, e.offsetY));
     }
   }
 });
 
-
 // Move the menu away when not needed
-const menu = select('.menu');
+const menu = select(".menu");
 
 const timeout = 5;
 let lastMoved = Date.now();
 
-document.addEventListener('mousemove', () => {
-  menu.classList.remove('moved');
+document.addEventListener("mousemove", () => {
+  menu.classList.remove("moved");
   lastMoved = Date.now();
-})
+});
 
 function hideMenuAfterInactivity() {
   const timeSinceMovement = Math.round((Date.now() - lastMoved) / 1000);
-  if(timeSinceMovement > timeout) menu.classList.add('moved');
+  if (timeSinceMovement > timeout) menu.classList.add("moved");
   window.requestAnimationFrame(hideMenuAfterInactivity);
 }
 
 hideMenuAfterInactivity();
-
 
 // resize the canvas when window is resized
 function resizeCanvas() {
@@ -127,17 +127,17 @@ function resizeCanvas() {
   canvas.height = window.innerHeight;
 }
 
-window.addEventListener('resize', () => {
+window.addEventListener("resize", () => {
   resizeCanvas();
-})
+});
 
 resizeCanvas();
 
 // clear all vehicles
-clearBtn.addEventListener('click', () => {
+clearBtn.addEventListener("click", () => {
   while (vehicles.length > 0) {
     vehicles.pop();
   }
   clearedRecently = true;
   clearTime = Date.now();
-})
+});
